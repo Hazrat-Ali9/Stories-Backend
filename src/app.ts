82cf48f3ab78486dae42012b8479d1c8e17037a5
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-30 23:06:19 +0600

    apps js 🥭

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import dotenv from "dotenv";
 
 import rootRouter from './modules/router'
 const app: Application = express()
-
+// apps js
 app.use(cors())
 app.use(express.json({ limit: '500mb' }))
 app.use(express.urlencoded({ extended: true, limit: '500mb' }))
