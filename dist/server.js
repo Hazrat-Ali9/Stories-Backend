c92a7317fd62721fa91d4abcd0cb4a61cf69120c
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-31 06:22:24 +0600

    ipmort default process 🦀

--- dist/server.js
+++ dist/server.js
@@ -2,6 +2,7 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+// Server js
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const main = async () => {
