06b9063670a9125326f51f98d6f99bb9b6e64902
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-11 02:00:15 +0600

    user controller :tada:

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import usersService from "./users.service";
-
+// user controller
 const create = async (req: Request, res: Response) => {
     try {
         const data = req.body
